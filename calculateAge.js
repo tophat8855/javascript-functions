29158bac9da51780201dcd250844e9bc03fdf474
tophat8855 <hrfarley@gmail.com>
2015-01-13 10:06:14 -0800

    Calculate age function

---
+++ calculateAge.js
@@ -0,0 +1,7 @@
+function calculateAge(birthYear) {
+  firstPossibility = 2015 - birthYear;
+  secondPossibility = firstPossibility - 1;
+  console.log("You are either " + secondPossibility + " or " + firstPossibility);
+}
+
+calculateAge(1997)
